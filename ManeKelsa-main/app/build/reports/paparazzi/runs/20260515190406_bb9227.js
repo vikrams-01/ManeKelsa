7e8bab7c7a35d603f88c179f593efef15ded8e89
vikrams-01 <vikrams-01@users.noreply.github.com>
2026-05-15 19:41:55 +0530

    Add all project files

---
+++ ManeKelsa-main/app/build/reports/paparazzi/runs/20260515190406_bb9227.js
@@ -0,0 +1,8 @@
+window.runs["20260515190406_bb9227"] = [
+  {
+    "testName": "com.manekelsa.PreviewTests#workerListScreenPreview",
+    "timestamp": "2026-05-15T13:34:27.371Z",
+    "tags": [],
+    "file": "images/bb4d85034b3084302fc529a1ecd36df395eb9c61.png"
+  }
+];
---
+++ ManeKelsa-main/app/build/reports/paparazzi/runs/20260515190433_77a340.js
@@ -0,0 +1,8 @@
+window.runs["20260515190433_77a340"] = [
+  {
+    "testName": "com.manekelsa.PreviewTests#workerProfileCardPreview",
+    "timestamp": "2026-05-15T13:34:35.145Z",
+    "tags": [],
+    "file": "images/58a6a87edfba684071ec9867b35476c6d50c5976.png"
+  }
+];
